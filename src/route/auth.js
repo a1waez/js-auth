7b00caeb70452ecdf9ddb9a9d75734a025518a17
Max <Maksim@Dell>
2024-08-23 16:34:28 +0300

    error

--- src/route/auth.js
+++ src/route/auth.js
@@ -4,6 +4,13 @@ const express = require('express')
 const router = express.Router()
 
 const { User } = require('../class/user')
+const { emit } = require('nodemon')
+
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  role: 1,
+})
 
 // ================================================================
 
@@ -46,6 +53,28 @@ router.get('/signup', function (req, res) {
     },
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрованний',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача',
+    })
+  }
 })
 
 // Підключаємо роутер до бек-енду
