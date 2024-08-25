d4c32dba142ed870e6d97e740a6f363642f0fad8
Max <Maksim@Dell>
2024-08-25 17:10:51 +0300

    end password recovery of js-auth

--- src/script/form.js
+++ src/script/form.js
@@ -78,16 +78,16 @@ export class Form {
     this.disabled = disabled
   }
 
-  // validateAll = () => {
-  //   Object.values(this.FIELD_NAME).forEach((name) => {
-  //     const error = this.validate(name, this.value[name])
+  validateAll = () => {
+    Object.values(this.FIELD_NAME).forEach((name) => {
+      const error = this.validate(name, this.value[name])
 
-  //     if (error) {
-  //       this.setError(name, error)
-  //       disabled = true
-  //     }
-  //   })
-  // }
+      if (error) {
+        this.setError(name, error)
+        disabled = true
+      }
+    })
+  }
 
   setAlert = (status, text) => {
     const el = document.querySelector(`.alert`)
